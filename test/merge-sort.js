79b70207f7d7280f116056397ec8490aa826640d
Ryan Bennett <ryanbennettvoid@gmail.com>
2018-04-27 21:35:52 -0700

    typo fix

--- test/merge-sort.js
+++ test/merge-sort.js
@@ -5,7 +5,7 @@ const {
   mergeSort
 } = require( '../' );
 
-test( 'quicksort #1', ( t ) => {
+test( 'merge sort #1', ( t ) => {
 
   const unsortedArr = [ 444, 5, 9, 10778, 6, 200, 998, 99, 55, 3, 22, 19 ];
   const expectedArr = [ 3, 5, 6, 9, 19, 22, 55, 99, 200, 444, 998, 10778 ];
